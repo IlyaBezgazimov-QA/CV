93323fc00bb0399863f3f47241297c2300079589
Ilya <127432949+IlyaBezgazimov-QA@users.noreply.github.com>
2023-03-10 20:47:09 +0300

    Add files via upload

---
+++ js.js
@@ -0,0 +1,5 @@
+const button = document.querySelector('button');
+
+button.addEventListener('click', function() {
+	alert('Отрадно спать – отрадней камнем быть. О, в этот век – преступный и постыдный – Не жить, не чувствовать – удел завидный. Прошу: молчи – не смей меня будить.')
+})
